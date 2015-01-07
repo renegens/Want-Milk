2cd5e94a7e11e734ba154834c7d1d032c6780146
Rene Gens <renegens@gmail.com>
2015-01-07 23:51:58 +0200

    Initial commit

---
+++ app/src/main/java/renegens/gotmilklocator/MapsActivity.java
@@ -0,0 +1,128 @@
+package renegens.gotmilklocator;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements renegens.gotmilklocator.LocationProvider.LocationCallback {
+
+    public static final String TAG = MapsActivity.class.getSimpleName();
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private LocationProvider mLocationProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+
+        mLocationProvider = new LocationProvider(this, this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        mLocationProvider.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        markerSetUp();
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    public void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+
+        /*Camera animation to location*/
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)             // Sets the center of the map to location user
+                .zoom(15)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().position(latLng).title(" You Are Here :) "));
+    }
+
+    //Setting up markers from array
+    public void markerSetUp() {
+
+        double[] gpsx = {39.636028, 39.642052, 39.626034, 39.624953, 39.626297, 39.636028, 39.6366,
+                39.640286, 39.633225, 39.642384, 39.632204, 39.631439, 39.63216, 39.650149, 39.6099};
+        double[] gpsy = {22.424397, 22.41044, 22.394707, 22.413641, 22.42174, 22.424397, 22.408838,
+                22.426468, 22.41593, 22.439586, 22.402893, 22.423186, 22.441789, 22.432785, 22.431465};
+        String[] arrayNames = getResources().getStringArray(R.array.storeArray);
+
+        for (int i = 0; i < gpsx.length; i++) {
+            double x = gpsx[i];
+            double y = gpsy[i];
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(x, y)).title(arrayNames[i]);
+            mMap.addMarker(marker);
+
+        }
+    }
+}
