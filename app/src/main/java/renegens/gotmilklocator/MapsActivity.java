2aa66932a557548e21920480c9c33e202f30283a
Rene Gens <renegens@gmail.com>
2015-01-08 23:14:18 +0200

    Location update
    Added location for Thessaloniki

--- app/src/main/java/renegens/gotmilklocator/MapsActivity.java
+++ app/src/main/java/renegens/gotmilklocator/MapsActivity.java
@@ -113,7 +113,7 @@ public class MapsActivity extends FragmentActivity implements renegens.gotmilklo
         //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
         MarkerOptions options = new MarkerOptions()
                 .position(latLng)
-                .title("I am here!");
+                .title(getResources().getString(R.string.my_position));
         mMap.addMarker(options);
 
         /*Camera animation to location*/
@@ -127,17 +127,26 @@ public class MapsActivity extends FragmentActivity implements renegens.gotmilklo
                 .tilt(40)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        mMap.addMarker(new MarkerOptions().position(latLng).title(" You Are Here :) "));
+        mMap.addMarker(new MarkerOptions().position(latLng).title(getResources().getString(R.string.my_position)));
     }
 
     //Setting up markers from array
     public void markerSetUp() {
 
-        double[] gpsx = {39.636028, 39.642052, 39.626034, 39.624953, 39.626297, 39.636028, 39.6366,
-                39.640286, 39.633225, 39.642384, 39.632204, 39.631439, 39.63216, 39.650149, 39.6099};
-        double[] gpsy = {22.424397, 22.41044, 22.394707, 22.413641, 22.42174, 22.424397, 22.408838,
-                22.426468, 22.41593, 22.439586, 22.402893, 22.423186, 22.441789, 22.432785, 22.431465};
+        double[] gpsx =
+                //For Larisa with pairs x and y and names from array with the same order
+                {39.641180, 39.642052, 39.626034, 39.624953, 39.626297, 39.636028, 39.6366,
+                39.640286, 39.633225, 39.642384, 39.632204, 39.631439, 39.63216, 39.650149, 39.6099,
+                /*For Thessloniki */
+                40.630591, 40.637867, 40.582404, 40.651851, 40.61476, 40.654166, 40.609253, 40.603881, 40.617316, 40.670667 };
+        double[] gpsy =
+                //For Larisa with pairs x and y and names from array with the same order
+                {22.418758, 22.41044, 22.394707, 22.413641, 22.42174, 22.424397, 22.408838,
+                22.426468, 22.41593, 22.439586, 22.402893, 22.423186, 22.441789, 22.432785, 22.431465,
+                /*For Thessloniki */
+                22.950781, 22.94882, 22.948664, 22.948162, 22.977683, 22.923416, 22.969673, 22.96865, 22.958589, 22.902774 };
         String[] arrayNames = getResources().getStringArray(R.array.storeArray);
+
 
         for (int i = 0; i < gpsx.length; i++) {
             double x = gpsx[i];
