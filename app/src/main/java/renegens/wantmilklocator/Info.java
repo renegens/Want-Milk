2e0cd994332ae00323850efa3cd789a817a23f41
Rene Gens <renegens@gmail.com>
2015-01-12 01:47:55 +0200

    new about section

--- app/src/main/java/renegens/wantmilklocator/Info.java
+++ app/src/main/java/renegens/wantmilklocator/Info.java
@@ -1,9 +1,11 @@
 package renegens.wantmilklocator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class Info extends ActionBarActivity {
@@ -12,6 +14,13 @@ public class Info extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ImageButton backButton = (ImageButton) findViewById(R.id.imageView);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 
