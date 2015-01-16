6207a40cb3a56f3078cf3442bc646ee0955a9b06
Rene Gens <renegens@gmail.com>
2015-01-16 04:25:50 +0200

    need to fix currentx = 0

--- app/src/main/java/renegens/wantmilklocator/MapsActivity.java
+++ app/src/main/java/renegens/wantmilklocator/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements renegens.wantmilklocator.LocationProvider.LocationCallback {
@@ -29,13 +30,13 @@ public class MapsActivity extends FragmentActivity implements renegens.wantmilkl
             {39.641180, 39.642052, 39.626034, 39.624953, 39.626297, 39.636028, 39.6366,
                     39.640286, 39.633225, 39.642384, 39.632204, 39.631439, 39.63216, 39.650149, 39.6099,
                 /*For Thessloniki */
-                    40.630591, 40.637867, 40.582404, 40.651851, 40.61476, 40.654166, 40.609253, 40.603881, 40.617316, 40.670667 };
+                    40.630591, 40.637867, 40.582404, 40.651851, 40.61476, 40.654166, 40.609253, 40.603881, 40.617316, 40.670667};
     double[] gpsy =
             //For Larisa with pairs x and y and names from array with the same order
             {22.418758, 22.41044, 22.394707, 22.413641, 22.42174, 22.424397, 22.408838,
                     22.426468, 22.41593, 22.439586, 22.402893, 22.423186, 22.441789, 22.432785, 22.431465,
                 /*For Thessloniki */
-                    22.950781, 22.94882, 22.948664, 22.948162, 22.977683, 22.923416, 22.969673, 22.96865, 22.958589, 22.902774 };
+                    22.950781, 22.94882, 22.948664, 22.948162, 22.977683, 22.923416, 22.969673, 22.96865, 22.958589, 22.902774};
 
     String[] arrayNames;
 
@@ -43,7 +44,9 @@ public class MapsActivity extends FragmentActivity implements renegens.wantmilkl
     double xLongtitude; //stores current location
     double yLatitude; // stores current location
 
-
+    double closestMarkerX; //stores closest location
+    double closestMarkerY; //stores closest location
+    int newIndex; //for the double arrays
 
 
     @Override
@@ -52,7 +55,7 @@ public class MapsActivity extends FragmentActivity implements renegens.wantmilkl
         setContentView(R.layout.activity_maps);
 
         //Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
-        ImageButton imageButton= (ImageButton) findViewById(R.id.imageButton);
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
 
 
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -60,11 +63,11 @@ public class MapsActivity extends FragmentActivity implements renegens.wantmilkl
             @Override
             public void onClick(View arg0) {
 
-                Intent browserIntent = new Intent(MapsActivity.this,Info.class);
+                Intent browserIntent = new Intent(MapsActivity.this, Info.class);
                 startActivity(browserIntent);
 
             }
-                });
+        });
         arrayNames = getResources().getStringArray(R.array.storeArray);
 
         setUpMapIfNeeded();
@@ -122,69 +125,101 @@ public class MapsActivity extends FragmentActivity implements renegens.wantmilkl
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        markerSetUp();
+
         distanceCalculator();
+        markerSetUp();
+
         //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
-    public void distanceCalculator(){
+
+
+    public void distanceCalculator() {
         double currentX = xLongtitude;
         double currentY = yLatitude;
-        double toCompareX = gpsx[0];
-        double toCompareY = gpsy[0];
-        double distance = distFrom(currentX,currentY,toCompareX,toCompareY);
-        double min = distance;
+        //double toCompareX = gpsx[0];
+        //double toCompareY = gpsy[0];
+        //double distance = distFrom(currentX,currentY,toCompareX,toCompareY);
+        double min = 500000000.00000;
         int index = 0;
 
         //points to compare
-        for (int i = 1; i<gpsx.length; i++){
-            for (int y = 0; y<gpsx.length; y++){
-                toCompareX = gpsx[y];
-                toCompareY = gpsy[y];
-                double result = distFrom(currentX,currentY,toCompareX,toCompareY);
 
-                if (result < min ) {
-                    min  = result;
-                    index = y;
-                }
+        for (int i = 0; i < gpsx.length; i++) {
+            double toCompareX = gpsx[i];
+            double toCompareY = gpsy[i];
 
+            double result = distFrom(currentX, currentY, toCompareX, toCompareY);
+
+            if (result < min) {
+                min = result;
+                index = i;
             }
+            Log.d("Method ", "currentx: " + currentX);
+            Log.d("Method ", "currenty: " + currentY);
+            Log.d("Method ", "min: " + min);
+            Log.d("Method ", "result: " + result);
+
         }
-        double closestMarkerX = gpsx[index];
-        double closestMarkerY = gpsy[index];
+
+        closestMarkerX = gpsx[index];
+        closestMarkerY = gpsy[index];
+        newIndex = index;
+
+
         //MarkerOptions marker = new MarkerOptions().position(new LatLng(closestMarkerX, closestMarkerY)).title("Closest");
 
         //mMap.addMarker(marker);
 
         //mMap.addMarker(new MarkerOptions().position(new LatLng(closestMarkerX, closestMarkerY).title(R.string()));
 
-        MarkerOptions marker = new MarkerOptions().position(new LatLng(closestMarkerX, closestMarkerY)).title(arrayNames[index]);
+        //MarkerOptions closestMarker = new MarkerOptions().position(new LatLng(closestMarkerX, closestMarkerY)).title(arrayNames[index]);
 
-        mMap.addMarker(marker).showInfoWindow();
+        //mMap.addMarker(closestMarker).showInfoWindow();
+
 
         Log.d("Debug", "Index: " + index);
         Log.d("Debug", "X: " + closestMarkerX);
         Log.d("Debug", "Y: " + closestMarkerY);
 
+
+    }
+
+
+    public double distFrom(double lat1, double lng1, double lat2, double lng2) {
+        {
+            double theta = lng1 - lng2;
+            double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+            dist = Math.acos(dist);
+            dist = rad2deg(dist);
+            dist = dist * 60 * 1.1515;
+            return (dist);
+        }}
+
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
 
 
-    public static float distFrom (double lat1, double lng1, double lat2, double lng2 )
-    {
-        double earthRadius = 3958.75;
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(lng2-lng1);
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-                        Math.sin(dLng/2) * Math.sin(dLng/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        double dist = earthRadius * c;
 
-        int meterConversion = 1609;
-
-        return new Float(dist * meterConversion).floatValue();
-    }
+//        double earthRadius = 3958.75;
+//        double dLat = Math.toRadians(lat2-lat1);
+//        double dLng = Math.toRadians(lng2-lng1);
+//        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+//                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+//                        Math.sin(dLng/2) * Math.sin(dLng/2);
+//        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//        double dist = earthRadius * c;
+//
+//        int meterConversion = 1609;
+//
+//        return new Float(dist * meterConversion).floatValue();
 
 
     public void handleNewLocation(Location location) {
@@ -209,14 +244,17 @@ public class MapsActivity extends FragmentActivity implements renegens.wantmilkl
 
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(latLng)             // Sets the center of the map to location user
-                .zoom(15)                   // Sets the zoom
+                .zoom(11)                   // Sets the zoom
                 .bearing(90)                // Sets the orientation of the camera to east
                 .tilt(40)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
         mMap.addMarker(new MarkerOptions().position(latLng).title(getResources().getString(R.string.my_position)).
-                icon(BitmapDescriptorFactory.fromResource(R.drawable.markercustom))).showInfoWindow();
+                icon(BitmapDescriptorFactory.fromResource(R.drawable.markercustom)));
+
+
+
 
 
     }
@@ -224,21 +262,20 @@ public class MapsActivity extends FragmentActivity implements renegens.wantmilkl
     //Setting up markers from array
     public void markerSetUp() {
 
-
-
-
         for (int i = 0; i < gpsx.length; i++) {
             double x = gpsx[i];
             double y = gpsy[i];
             MarkerOptions marker = new MarkerOptions().position(new LatLng(x, y)).title(arrayNames[i]);
-
             mMap.addMarker(marker);
 
 
-
-
-
+            Marker closestMarker = mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(closestMarkerX, closestMarkerY))
+                    .title(arrayNames[newIndex]));
+            closestMarker.showInfoWindow();
         }
+        Log.d("Debug", "NewIndexValue:" + newIndex);
+
     }
 
 }
